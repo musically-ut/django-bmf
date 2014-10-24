58e61faef249763ee784c730868c37afe8f10ad6
Sebastian Braun <sebastian@elmnt.de>
2014-10-24 04:05:51 +0200

    added migrations for new version

---
+++ djangobmf/contrib/employee/migrations/0004_version_0_2_0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('djangobmf_employee', '0003_optional_employee_product'),
+    ]
+
+    operations = [
+    ]
