812773803ab7ffbe384a6229f72009f73ea5028e
Sebastian Braun <sebastian@elmnt.de>
2014-11-07 10:01:00 +0100

    pep8

--- docs/_ext/djangobmfdocs.py
+++ docs/_ext/djangobmfdocs.py
@@ -1,8 +1,8 @@
-def setup(app):                                                                
-    app.add_crossref_type(                                                     
-        directivename = "setting",                                             
-        rolename      = "setting",                                             
-        indextemplate = "pair: %s; setting",                                   
+def setup(app):
+    app.add_crossref_type(
+        directivename = "setting",
+        rolename      = "setting",
+        indextemplate = "pair: %s; setting",
     )
     app.add_crossref_type(
         directivename = "templatetag",
