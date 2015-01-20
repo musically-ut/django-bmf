aabe699301bc9bdeb31dbe9666eeaf01981d427b
Sebastian Braun <sebastian@elmnt.de>
2015-01-20 14:46:52 +0100

    updated tests

--- djangobmf/contrib/address/tests.py
+++ djangobmf/contrib/address/tests.py
@@ -17,31 +17,26 @@ class AddressFactory(ModuleTestFactory, DemoDataMixin, TestCase):
     app = AddressConfig
 
 
-class AddressModuleTests(ModuleMixin, TestCase):
+class AddressModuleTests(ModuleMixin, DemoDataMixin, TestCase):
 
     def test_urls_user(self):
         """
         """
         self.model = Address
 
-#       data = self.autotest_ajax_get('create', kwargs={'key': 'default'})
-#       data = self.autotest_ajax_post('create', kwargs={'key': 'default'}, data={
-#           'customer': 1,
-#           'name': 'name',
-#           'street': 'street 12',
-#           'zip': '24342',
-#           'city': 'city',
-#           'state': 'state',
-#           'country': 'country',
-#           'is_active': '1',
-#       })
-#       self.assertNotEqual(data["object_pk"], 0)
-#       self.autotest_get('index', 200)
+        data = self.autotest_ajax_post('create', kwargs={'key': 'default'}, data={
+            'customer': 1,
+            'name': 'name',
+            'street': 'street 12',
+            'zip': '24342',
+            'city': 'city',
+            'state': 'state',
+            'country': 'country',
+            'is_active': '1',
+        })
+        self.assertNotEqual(data["object_pk"], 0)
 
-#       obj = self.get_latest_object()
-#       a = '%s'%obj # check if object name has any errors
+        obj = self.get_latest_object()
+        a = '%s'%obj # check if object name has any errors
 
-#       self.autotest_get('detail', kwargs={'pk': obj.pk}, api=False)
-#       data = self.autotest_ajax_get('update', kwargs={'pk': obj.pk})
-#       self.autotest_get('delete', kwargs={'pk': obj.pk})
-#       self.autotest_post('delete', status_code=302, kwargs={'pk': obj.pk})
+        self.autotest_post('delete', status_code=302, kwargs={'pk': obj.pk})
