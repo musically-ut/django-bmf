0b8cff4a9c5614efad6905fdf2ddc5f25bab81dd
Sebastian Braun <sebastian@elmnt.de>
2015-02-07 19:27:38 +0100

    removed test dependency factory_boy

--- djangobmf/contrib/invoice/tests.py
+++ djangobmf/contrib/invoice/tests.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 from django.test import LiveServerTestCase
 from django.core.urlresolvers import reverse
 
-from factory.django import DjangoModelFactory
 from unittest import expectedFailure
 
 from .apps import InvoiceConfig
@@ -22,18 +21,6 @@ from djangobmf.utils.testcases import ModuleTestFactory
 
 class InvoiceFactory(ModuleTestFactory, DemoDataMixin, TestCase):
     app = InvoiceConfig
-
-
-class InvoiceFactory(DjangoModelFactory, TestCase):
-    class Meta:
-        model = Invoice
-   #customer = 1
-   #project = 1
-   #date = '2014-01-01'
-   #invoice_address = 1
-   #shipping_address = 1
-   #employee = 1
-   #invoice_number = "TEST INVOICE"
 
 
 class InvoiceModuleTests(ModuleMixin, TestCase):
