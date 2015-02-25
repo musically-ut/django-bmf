bd350bd29ea6bef63e49c88288b5b805a0d01c3d
Sebastian Braun <sebastian@elmnt.de>
2015-02-25 23:28:11 +0100

    update invoice and quotation templates

--- djangobmf/contrib/invoice/serializers.py
+++ djangobmf/contrib/invoice/serializers.py
@@ -5,9 +5,20 @@ from __future__ import unicode_literals
 
 from djangobmf.serializers import ModuleSerializer
 
+from rest_framework import serializers
+
 from .models import Invoice
 
 
 class InvoiceSerializer(ModuleSerializer):
+    project_name = serializers.ReadOnlyField(source='project.name')
+
     class Meta:
         model = Invoice
+        fields = (
+            'invoice_number',
+            'state',
+            'project',
+            'project_name',
+            'bmfdetail',
+        )
--- djangobmf/contrib/quotation/serializers.py
+++ djangobmf/contrib/quotation/serializers.py
@@ -5,10 +5,20 @@ from __future__ import unicode_literals
 
 from djangobmf.serializers import ModuleSerializer
 
+from rest_framework import serializers
+
 from .models import Quotation
 
 
 class QuotationSerializer(ModuleSerializer):
+    project_name = serializers.ReadOnlyField(source='project.name')
 
     class Meta:
         model = Quotation
+        fields = (
+            'quotation_number',
+            'state',
+            'project',
+            'project_name',
+            'bmfdetail',
+        )
